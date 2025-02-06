2efe2082032339847170898bd202053c7edfb1ea
SaanyaGarg01 <saanyagarg800@gmail.com>
2025-02-06 22:28:30 +0530

    Add files via upload

---
+++ digital-footprint/footprint.js
@@ -0,0 +1,5 @@
+function trackFootprint() {
+    const feedback = document.getElementById('footprint-feedback');
+    feedback.innerText = "Your digital footprint includes everything you post online. Be mindful of what you share!";
+    feedback.style.color = "blue";
+}
